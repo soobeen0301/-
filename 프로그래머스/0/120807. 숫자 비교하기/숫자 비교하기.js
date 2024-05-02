6a822148c6fb358db7458ee6275d9e4eb1385741
soobeen0301 <167050632+soobeen0301@users.noreply.github.com>
2024-05-02 09:29:29 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,12 @@
+function solution(num1, num2) {
+    var answer = 0;
+
+    if (num1 === num2) {
+        answer = 1;
+    } else {
+        answer = -1;
+    }
+    return answer;
+}
+
+console.log(solution(11,11));
