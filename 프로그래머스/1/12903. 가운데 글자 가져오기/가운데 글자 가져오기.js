bf982c782a4dff72a1c9644e4ce1fbb2ef146886
soobeen0301 <167050632+soobeen0301@users.noreply.github.com>
2024-05-14 09:26:26 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    var answer = '';
+
+    if (s.length % 2 === 0) {
+        answer = s[s.length / 2 -1] + s[s.length / 2];
+    } else {
+        answer = s[Math.floor(s.length/2)];
+    }
+
+    return answer;
+}
+
+console.log(solution('abcde'));
+console.log(solution('qwer'));
