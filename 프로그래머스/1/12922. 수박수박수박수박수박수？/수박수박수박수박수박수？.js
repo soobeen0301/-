1b0520aad544903abfda32808bf7bd09324bc5de
soobeen0301 <167050632+soobeen0301@users.noreply.github.com>
2024-05-16 09:18:49 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = '';
+
+    for(let i = 1; i <= n; i++) {
+        if (i % 2 === 0) {
+            answer += "박";
+        }else {
+            answer += "수";
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution(3));
+console.log(solution(4));
