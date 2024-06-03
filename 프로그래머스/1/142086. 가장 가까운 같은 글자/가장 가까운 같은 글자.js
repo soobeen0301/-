0d6574512e3a3e1513708a3947f05454c1fd9bed
soobeen0301 <167050632+soobeen0301@users.noreply.github.com>
2024-06-03 09:22:28 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 10.26 ms, Memory: 38.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,17 @@
+function solution(s) {
+  var answer = [];
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (!stack.includes(s[i])) {
+      answer.push(-1);
+      stack.push(s[i]);
+      continue;
+    } else {
+      answer.push(stack.length - stack.lastIndexOf(s[i]));
+      stack.push(s[i]);
+      continue;
+    }
+  }
+  return answer;
+}
