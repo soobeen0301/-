8e0714b503c14d86d55a76682f435dfafaf05380
soobeen0301 <167050632+soobeen0301@users.noreply.github.com>
2024-06-24 09:54:34 +0900

    [level 0] Title: 두 수의 차, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
@@ -1,9 +1,8 @@
 function solution(num1, num2) {
     var answer = 0;
-
-    if(num1 >= -50000 && num1 <= 50000 && num2 >= -50000 && num2 <= 50000) {
-        answer = num1 - num2;
-    }
+    const num = num1 - num2
+    
+    answer = num
     
     return answer;
 }
