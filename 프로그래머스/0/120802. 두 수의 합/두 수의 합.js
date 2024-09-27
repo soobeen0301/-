6599d9c370e940b0a3acc916a83caa7aca344ae7
soobeen0301 <167050632+soobeen0301@users.noreply.github.com>
2024-09-27 15:49:09 +0900

    [level 0] Title: 두 수의 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.js
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.js
@@ -1,6 +1,4 @@
-function solution(num1, num2) {
-    var answer = 0;
-    answer = num1 + num2;
+function solution(num1 , num2) {
+    const answer = num1 + num2
     return answer;
 }
-console.log(solution(100,2));
